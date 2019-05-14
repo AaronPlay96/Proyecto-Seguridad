01070dd58f647c7e3796004c711cd84de7c1aa80
AaronPlay96 <betancourt199655@gmail.com>
2019-05-14 15:19:24 -0400

    añadido_de_base_de_datos

---
+++ database/python/db_seguridad.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+db_seguridad = '..\sqlite\database_seguridad.db'
+
+
+def insert_alert(aid,s,ad,atm):
+    conn = sqlite3.connect(db_seguridad)
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO ALERT_REGISTRY (ALERT_ID, SENSOR, ALERT_DATE, ALERT_TIME)VALUES (" + aid + "," + s + "," + ad + "," + atm + ");")
+
+    conn.commit()
+    conn.close()
+
+
+def insert_connection(cd, ct):
+    conn = sqlite3.connect(db_seguridad)
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO ALERT_REGISTRY (CONNECTION_DATE, CONNECTION_TIME)VALUES (" + cd + "," + ct + ");")
+
+    conn.commit()
+    conn.close()
+
+
+def get_connections():
+    con_lst = []
+    conn = sqlite3.connect(db_seguridad)
+    cursor = conn.cursor()
+
+    for row in cursor.execute("SELECT * from CONNECTIONS"):
+        con_lst.append([row[1],row[2]])
+
+    conn.commit()
+    conn.close()
+    return con_lst
+
+def get_alerts_by_type(ty):
+    ale_lst = []
+    conn = sqlite3.connect(db_seguridad)
+    cursor = conn.cursor()
+
+    for row in cursor.execute("SELECT * from (SELECT * from ALERT_REGISTRY CROSS JOIN ALERT_TYPE) where ALERT_TYPE LIKE " + str(ty)):
+        ale_lst.append([row[1], row[2]])
+
+    conn.commit()
+    conn.close()
+    return ale_lst
